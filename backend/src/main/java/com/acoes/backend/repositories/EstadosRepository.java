97bc05c125d3f0c3ffe0a7aa39e8e03417f1d0e5
glaucoSapucaia <glaucopco@gmail.com>
2026-02-15 22:03:33 -0300

    create: backend estados + insomnia

---
+++ backend/src/main/java/com/acoes/backend/repositories/EstadosRepository.java
@@ -0,0 +1,13 @@
+package com.acoes.backend.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.acoes.backend.models.Estados;
+
+@Repository
+public interface EstadosRepository extends JpaRepository<Estados, Long> {
+    List<Estados> findByCodigoIbge(Integer codigoIbge);
+}
